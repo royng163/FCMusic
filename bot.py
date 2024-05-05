00e11787ae6a92b006c9435274a76017410b091b
NCFCRoy <ncfcroy@gmail.com>
2024-05-05 15:20:37 +0800

    remove auto disconnect

--- bot.py
+++ bot.py
@@ -22,13 +22,9 @@ async def on_ready():
 async def on_wavelink_node_ready(payload: wavelink.NodeReadyEventPayload):
     print(f"Node {payload.node.identifier} is ready!")
 
-@bot.event
-async def on_wavelink_inactive_player(player: wavelink.Player):
-    await player.disconnect()
-
 @bot.tree.command(name="play", description="Play the song")
 async def play(interaction: Interaction, url: str=None):
-    await interaction.response.defer(thinking=True)
+    await interaction.response.defer()
     vClient = interaction.guild.voice_client
 
     if (url is None):
@@ -80,7 +76,7 @@ async def play(interaction: Interaction, url: str=None):
 
 @bot.tree.command(name="pause", description="Pause the song")
 async def pause(interaction: Interaction):
-    await interaction.response.defer(thinking=True)
+    await interaction.response.defer()
     vClient = interaction.guild.voice_client
 
     if (vClient is None):
@@ -98,7 +94,7 @@ async def pause(interaction: Interaction):
 
 @bot.tree.command(name="queue", description="Display the queue")
 async def queue(interaction: Interaction):
-    await interaction.response.defer(thinking=True)
+    await interaction.response.defer()
     vClient = interaction.guild.voice_client
 
     if (vClient is None):
@@ -117,7 +113,7 @@ async def queue(interaction: Interaction):
 
 @bot.tree.command(name="nowplaying", description="Display the current song")
 async def nowplaying(interaction: Interaction):
-    await interaction.response.defer(thinking=True)
+    await interaction.response.defer()
     vClient = interaction.guild.voice_client
 
     if (vClient is None):
@@ -135,7 +131,7 @@ async def nowplaying(interaction: Interaction):
 
 @bot.tree.command(name="skip", description="Skip the song")
 async def skip(interaction: Interaction):
-    await interaction.response.defer(thinking=True)
+    await interaction.response.defer()
     vClient = interaction.guild.voice_client
 
     if (vClient is None):
@@ -150,7 +146,7 @@ async def skip(interaction: Interaction):
 
 @bot.tree.command(name="shuffle", description="Shuffle the queue")
 async def shuffle(interaction: Interaction):
-    await interaction.response.defer(thinking=True)
+    await interaction.response.defer()
     vClient = interaction.guild.voice_client
 
     if (vClient is None):
@@ -162,7 +158,7 @@ async def shuffle(interaction: Interaction):
 
 @bot.tree.command(name="clear", description="Clear the queue")
 async def clear(interaction: Interaction):
-    await interaction.response.defer(thinking=True)
+    await interaction.response.defer()
     vClient = interaction.guild.voice_client
 
     if (vClient is None):
@@ -174,7 +170,7 @@ async def clear(interaction: Interaction):
 
 @bot.tree.command(name="playlist", description="Display updated playlist")
 async def playlist(interaction: Interaction, url: str):
-    await interaction.response.defer(thinking=True)
+    await interaction.response.defer()
 
     try:
         playlist = await wavelink.Playable.search(url)
@@ -192,7 +188,7 @@ async def playlist(interaction: Interaction, url: str):
 
 @bot.tree.command(name="stop", description="Terminate the player")
 async def stop(interaction: Interaction):
-    await interaction.response.defer(thinking=True)
+    await interaction.response.defer()
     vClient = interaction.guild.voice_client
 
     if (vClient is None):
